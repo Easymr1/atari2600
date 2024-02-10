a8e988be4d463b6cc1a7d219ff14b3bc9ddff34d
Easymr1 <mimoun06270@hotmail.fr>
2024-02-10 17:34:29 -0500

    draw object on the screen

---
+++ screenobject/screenobject.asm
@@ -0,0 +1,192 @@
+        processor 6502
+
+        include "vcs.h"
+        include "macro.h"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start our ROM code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	seg 
+        org $f000
+
+Reset:
+	CLEAN_START
+        
+        ldx #$09
+        stx COLUBK
+        
+        lda #$44
+        sta COLUPF
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start a new frame by configuring VBLANK and VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+StartFrame:
+	lda #$2
+        sta VBLANK
+        sta VSYNC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Generate the three line of VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	REPEAT 3
+        sta WSYNC
+        REPEND
+        lda #0
+        sta VSYNC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Let the TIA output the 37 recommended line of VBLANK
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        REPEAT 37
+        	sta WSYNC
+        REPEND
+        lda #0
+        sta VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set the CTRLPF register to allow playfield reflection 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	ldx #%00000000
+    stx CTRLPF
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set color players 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #$57
+    sta COLUP0
+
+    lda #$BB
+    sta COLUP1
+
+    lda #%00000010
+    sta CTRLPF
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Draw 192 visible scanlines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Init 10 line with nothing 
+VisibleScanlines:
+    REPEAT 10
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display 10 line for the scorboard using an array of bytes 
+;; defined at NumberBitMap
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ldy #0                 
+ScoreBoardLoop:
+    lda NumberBitMap,Y  
+    sta PF1
+    sta WSYNC
+    iny
+    cpy #10
+    bne ScoreBoardLoop
+
+    lda #0
+    sta PF1
+
+
+; Droaw 50 line with nothing 
+    REPEAT 50
+        sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display 10 line for the player 0 using an array of bytes 
+;; defined at PlayerBitMap
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ldy #0                 
+Player0Loop:
+    lda PlayerBitMap,Y  
+    sta GRP0
+    sta WSYNC
+    iny
+    cpy #10
+    bne Player0Loop
+
+    lda #0
+    sta GRP0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display 10 line for the player 1 using an array of bytes 
+;; defined at PlayerBitMap
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ldy #0                 
+Player1Loop:
+    lda PlayerBitMap,Y  
+    sta GRP1
+    sta WSYNC
+    iny
+    cpy #10
+    bne Player1Loop
+
+    lda #0
+    sta GRP1
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Draw the reste of the scanline (192 - 90)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    REPEAT 102
+        sta WSYNC
+    REPEND
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Output 30 more VBLANK overscan lines to complete our frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	lda #2
+    sta VBLANK
+    REPEAT 30
+        sta WSYNC
+    REPEND
+    lda #0
+    sta VBLANK
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Loop to next frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	jmp StartFrame
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Player
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $ffe8
+PlayerBitMap:
+    .byte #%01111110       ;  ######
+    .byte #%11111111       ; ########
+    .byte #%10011001       ; #  ##  #
+    .byte #%11111111       ; ########
+    .byte #%11100111       ; ###  ###
+    .byte #%11111111       ; ########
+    .byte #%10111101       ; # #### #
+    .byte #%11000011       ; ##    ##
+    .byte #%11000011       ; ########
+    .byte #%01111110       ;  ######
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Score
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $fff2
+NumberBitMap:
+    .byte #%00001110      ; ########
+    .byte #%00001110      ; ########
+    .byte #%00000010      ;      ###
+    .byte #%00000010      ;      ###
+    .byte #%00001110      ; ########
+    .byte #%00001110      ; ########
+    .byte #%00001000      ; ###
+    .byte #%00001000      ; ###
+    .byte #%00001110      ; ########
+    .byte #%00001110      ; ########
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Epilogue
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	org $fffc
+        .word Reset	; reset vector
+        .word Reset	; BRK vector
